16d6bd1a212a97510a20dbd4581882b5175e5a12
WangRui <wangruihit@gmail.com>
2016-05-26 17:01:07 +0800

    add constants.h

---
+++ Chatchat/constants.h
@@ -0,0 +1,16 @@
+//
+//  constants.h
+//  Chatchat
+//
+//  Created by wangruihit@gmail.com on 5/26/16.
+//  Copyright © 2016 Beta.Inc. All rights reserved.
+//
+
+#ifndef constants_h
+#define constants_h
+
+#import <Foundation/Foundation.h>
+
+NSString *const kServerURL = @"http://192.168.127.241:3000";
+
+#endif /* constants_h */
